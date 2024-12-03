cf55748624683dff32dc3b585b8ae1f06fa54477
FATHIMA FIDHA <spk610520@gmail.com>
2024-12-03 10:00:22 +0530

    Button work

---
+++ work2/button.js
@@ -0,0 +1,13 @@
+document.getElementById("first").addEventListener("click",function a() {
+    document.getElementById("first").style.backgroundColor="green"
+})
+
+
+document.getElementById("second").addEventListener("click",function b() {
+    document.getElementById("second").style.backgroundColor="red"
+})
+
+const third=document.getElementById("third")
+third.addEventListener("click",function c(){
+     third.style.backgroundColor="red"
+})
